9837d611334b14dc1c9e24417c9ef7aafc5c1474
Subhankar Sarkar <93681750+sarkarsubho@users.noreply.github.com>
2024-05-17 19:19:22 +0530

    add cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,59 +1,55 @@
 const express = require("express");
 const mysql = require("mysql");
-
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
+app.use(cors({
+    origin: "http://localhost:5173"
+  }));
 
 const database = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "W7301@jqir#",
-    database: "sys",
+  host: "localhost",
+  user: "root",
+  password: "W7301@jqir#",
+  database: "sys",
+});
+
+database.connect(() => {
+  console.log("database connected");
+});
+
+// Create a route to retrieve all favourites
+app.get("/api/favourites", (req, res) => {
+  const query = "SELECT * FROM favourites";
+  db.query(query, (err, results) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Error retrieving favourites");
+    } else {
+      res.json(results);
+    }
   });
+});
 
-  database.connect(()=>{
-console.log("database connected")
-  })
-
-  // Create a route to retrieve all favourites
-app.get('/api/favourites', (req, res) => {
-    const query = 'SELECT * FROM favourites';
-    db.query(query, (err, results) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send('Error retrieving favourites');
-      } else {
-        res.json(results);
-      }
-    });
+// Create a route to add a new favourite
+app.post("/api/favourites", (req, res) => {
+  const { title, year } = req.body;
+  const query = "INSERT INTO favourites (title, year) VALUES (?, ?)";
+  db.query(query, [title, year], (err, results) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Error adding favourite");
+    } else {
+      res.json({ message: "Favourite added successfully" });
+    }
   });
-  
-  // Create a route to add a new favourite
-  app.post('/api/favourites', (req, res) => {
-    const { title, year } = req.body;
-    const query = 'INSERT INTO favourites (title, year) VALUES (?, ?)';
-    db.query(query, [title, year], (err, results) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send('Error adding favourite');
-      } else {
-        res.json({ message: 'Favourite added successfully' });
-      }
-    });
-  });
-  
-// app.post("/api/favourites",(req,res)=>{
-//     res.send("this is home route");
+});
 
-// })
-// app.get("/api/favourites",(req,res)=>{
-//     res.send("this is home route");
-// })
-app.get("/",(req,res)=>{
-    res.send("this is home route");
-})
+app.get("/", (req, res) => {
+  res.send("this is home route");
+});
 
-app.listen(8080,()=>{
-    console.log("server is connected")
-})
+app.listen(8080, () => {
+  console.log("server is connected");
+});
